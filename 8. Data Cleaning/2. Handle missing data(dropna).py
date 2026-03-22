a490cd99fa43cb92c17f7cc95d6f56a9910654af
Sravan 7 <thesravann7@gmail.com>
2026-03-22 21:20:27 +0530

    Update 2. Handle missing data(dropna).py

--- 8. Data Cleaning/2. Handle missing data(dropna).py
+++ 8. Data Cleaning/2. Handle missing data(dropna).py
@@ -13,12 +13,3 @@ df = df.dropna(subset=['Type2'])
 
 print(df.to_string())
 print('---------------------------------------')
-
-'''IN PLACE OF DROPPING ANY ROWS WHERE THE VALUES ARE NOT AVALIABLE, LET'S REPLACE THEM INSTEAD WITH A DIFFERENT FUNCTION'''
-
-df = df.fillna({'Type2' :'None' })
-
-# fillna = FILL ANY NOT AVALIABLE VALUES
-
-print(df.to_string())
-print('----------------------------------------')
